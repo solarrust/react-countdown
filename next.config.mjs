bb4e1daef3dc8d63f6de71fc5fe672e5e9cb337b
Alena Batitskaia <batickaya.a@gmail.com>
2024-09-18 17:06:33 +0100

    Next configuration added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/react-countdown",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
