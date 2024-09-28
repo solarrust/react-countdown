449607553aa233d93a3173274983b5bb7f86863d
Alena Batitskaia <batickaya.a@gmail.com>
2024-09-28 13:16:40 +0100

    Fix balanceTime test

--- src/app/modules/balanceTime.test.js
+++ src/app/modules/balanceTime.test.js
@@ -1,75 +1,39 @@
-import { expect, describe, it } from 'vitest';
-import balanceTime from '../modules/balanceTime';
+import { expect, describe, it } from "vitest";
+import balanceTime from "../modules/balanceTime";
 
 describe.each([
   {
     input: {
       hours: 0,
       minutes: 0,
-      seconds: 10
+      seconds: 10,
     },
     output: {
       hours: 0,
       minutes: 0,
-      seconds: 10
-    }
-  },
-  {
-    input: {
-      hours: 0,
-      minutes: 0,
-      seconds: 100
+      seconds: 10,
     },
-    output: {
-      hours: 0,
-      minutes: 1,
-      seconds: 40
-    }
-  },
-  {
-    input: {
-      hours: 0,
-      minutes: 100,
-      seconds: 10
-    },
-    output: {
-      hours: 1,
-      minutes: 40,
-      seconds: 10
-    }
-  },
-  {
-    input: {
-      hours: 100,
-      minutes: 10,
-      seconds: 10
-    },
-    output: {
-      hours: 100,
-      minutes: 10,
-      seconds: 10
-    }
   },
   {
     input: {
       hours: 100,
       minutes: 100,
-      seconds: 100
+      seconds: 100,
     },
     output: {
       hours: 101,
       minutes: 41,
-      seconds: 40
-    }
-  }
-])('timeBalancer function test', ({ input, output }) => {
-  it(`Expect if input is ${(JSON.stringify(input))} then result will be equals to ${(JSON.stringify(output))}`, () => {
-    expect(balanceTime(input)).toEqual(output)
-  })
-})
+      seconds: 40,
+    },
+  },
+])("timeBalancer function test", ({ input, output }) => {
+  it(`Expect if input is ${JSON.stringify(input)} then result will be equals to ${JSON.stringify(output)}`, () => {
+    expect(balanceTime(input)).toEqual(output);
+  });
+});
 
 describe('Throwing Error if argument didn’t contain "minuets" or "seconds" keys', () => {
   it("Should throw an Error if no required keys were found in object", () => {
-    expect(() => balanceTime({ hours: 0, minutes: 10 })).toThrow()
-  })
-})
+    expect(() => balanceTime({ hours: 0, minutes: 10 })).toThrow();
+  });
+});
